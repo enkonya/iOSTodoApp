c6ac6425a992a9b2b50fb73c9b9c4d2e6ea68167
Ellen Nkonya <enkonyaa@bitbucket.org>
2019-01-03 17:11:07 -0500

    MOB-1411 - created segmented list for open/completed tasks, sorting tasks in descending dateAdded order, also added empty states

--- TodoApp/Controllers/MainViewController.swift
+++ TodoApp/Controllers/MainViewController.swift
@@ -10,72 +10,132 @@ import UIKit
 import RealmSwift
 
 class MainViewController: UITableViewController{
+    var notificationToken: NotificationToken? = nil
+    var results: Results<ToDoListItem>?
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    @IBOutlet weak var segmentedControl: UISegmentedControl?
+    
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex
+        {
+        case 0:
+            setupRealmDataSource(false)
+        case 1:
+            setupRealmDataSource(true)
+        default:
+            break
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidLoad()
+        setupRealmDataSource(false)
+      }
+    
+    func setupRealmDataSource(_ isComplete: Bool) {
         do{
             let realm = try Realm()
-            return realm.objects(ToDoListItem.self).count
+            results = realm.objects(ToDoListItem.self).filter("isComplete = %@", isComplete)
+                                                      .sorted(byKeyPath: "dateEntered", ascending: false)
+            
+            // Observe Results Notifications
+            notificationToken = results?.observe { [weak self] (changes: RealmCollectionChange) in
+                guard let tableView = self?.tableView else { return }
+                switch changes {
+                case .initial:
+                    // Results are now populated and can be accessed without blocking the UI
+                    tableView.reloadData()
+                case .update(_, let deletions, let insertions, let modifications):
+                    // Query results have changed, so apply them to the UITableView
+                    let fromRow = { (row: Int) in
+                        return IndexPath(row: row, section: 0)
+                    }
+                    
+                    tableView.beginUpdates()
+                    tableView.insertRows(at: insertions.map(fromRow), with: .automatic)
+                    tableView.reloadRows(at: modifications.map(fromRow), with: .automatic)
+                    tableView.deleteRows(at: deletions.map(fromRow), with: .automatic)
+                    tableView.endUpdates()
+                case .error(let error):
+                    // An error occurred while opening the Realm file on the background worker thread
+                    fatalError("\(error)")
+                }
+            }
         }
         catch{
             print("Unable to initialize realm")
-            return 0;
         }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        notificationToken?.invalidate()
+    }
+        
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let count = results?.count ?? 0
+        
+        if(count == 0){
+            let emptyLabel = UILabel(frame: CGRect(x: 0,
+                                                   y: 0,
+                                                   width:  self.view.frame.width,
+                                                   height: self.view.frame.height))
+            emptyLabel.text = String(format: "No %@ Tasks",
+                                     segmentedControl?.selectedSegmentIndex == 0 ? "Open" : "Completed")
+            emptyLabel.textAlignment = NSTextAlignment.center
+            
+            tableView.backgroundView = emptyLabel
+            tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+        }
+        else{
+            tableView.backgroundView = nil
+            tableView.separatorStyle = UITableViewCell.SeparatorStyle.singleLine
+        }
+        
+        return count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for:indexPath)
+        let item = results?[indexPath.row]
+        cell.textLabel?.text = item?.name
+        cell.accessoryType = item?.isComplete == true ? .checkmark : .none
         
-        do{
-            let realm = try Realm()
-            let item = realm.objects(ToDoListItem.self)[indexPath.row]
-            cell.textLabel!.text = item.name
-            cell.accessoryType = item.done == true ? .checkmark : .none
-        }
-        catch{
-            print("Unable to initialize realm")
-        }
         return cell
     }
     
     //marks and unmarks as done
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        do{
-            let realm = try Realm()
-            let item = realm.objects(ToDoListItem.self)[indexPath.row]
-            
-            try realm.write{
-                item.done = !item.done
+        if let item = results?[indexPath.row]{
+            do{
+                let realm = try Realm()
+                try realm.write{
+                    item.isComplete = !item.isComplete
+                }
+                tableView.deselectRow(at: indexPath, animated: true)
             }
-            
-            tableView.deselectRow(at: indexPath, animated: true)
-            tableView.reloadRows(at: [indexPath], with: .automatic)
-        }
-        catch{
-            print("Unable to initialize realm")
+            catch{
+                print("Unable to initialize realm")
+            }
         }
     }
     
     //ability to edit row in table views
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true;
+        return true
     }
     
     //if the editing style is set to delete, create an item that represents the cell we want to delete
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        do{
-            if(editingStyle == .delete){
+        if let item = results?[indexPath.row], editingStyle == .delete{
+            do{
                 let realm = try Realm()
-                let item = realm.objects(ToDoListItem.self)[indexPath.row]
-                
                 try realm.write{
                     realm.delete(item)
                 }
-                
-                tableView.deleteRows(at: [indexPath], with: .automatic)
             }
-        }
-        catch{
-           print("Unable to initialize realm")
+            catch{
+                print("Unable to initialize realm")
+            }
         }
     }
 
@@ -94,15 +154,14 @@ class MainViewController: UITableViewController{
             //creating a new item to add to realm
             let newTodoListItem = ToDoListItem()
             newTodoListItem.name = todoItemTextField?.text ?? ""
-            newTodoListItem.done = false
+            newTodoListItem.isComplete = false
+            newTodoListItem.dateEntered = Date()
             
             do{
                 let realm = try Realm()
                 try realm.write {
                     realm.add(newTodoListItem) //adding to realm
                 }
-                let count = realm.objects(ToDoListItem.self).count - 1
-                self.tableView.insertRows(at: [IndexPath.init(row: count, section: 0)], with: .automatic)
             }
             catch{
                 print("Unable to initialize realm")
